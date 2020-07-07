ed930e9e04c67060ddc7f5ed87946d37d4676b6e
crawfordj <Jenny.Crawford@utiligroup.com>
2020-07-07 10:59:37 +0100

    added failing unit test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -40,6 +40,16 @@ public class AppTest
         }
     }
 
+    @Test
+    public void testFail()
+    {
+        App.main(null);
+        try {
+            assertEquals("FailureMessage!" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World!\"");
+        }    }
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
